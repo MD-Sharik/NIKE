58ef06babb30511d4ddaa53ef20e5279159c2b4e
Sharik <md.sharik5012@gmail.com>
2023-12-13 22:12:19 +0530

    Initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <div id="block1">
+      <img src="bg3.png" className="img" />
+      <h1 id="intro">NIKE</h1>
     </div>
+    <div id="block2">
+      <p>INVEST IN <br/>
+      GREATNESS</p>
+    </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+      <div className='navbar'>
+        <ul><li>PRODUCT</li>
+        <li>NFT</li>
+        <li>FAQ</li>
+        <li>FOUNDER</li></ul>
+    </div>
+  )
+}
